2dbb9641c71f63732a89dd80505473a04c26105d
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2014-03-17 22:01:41 -0700

    got one test to pass

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -4,16 +4,41 @@ require './stack.rb'
 
 class RPNCalculator
  
-  # Parse should return a list class that you defined, not a ruby array 
   def self.parse(rpn_string)
+    arr = rpn_string.split(" ")
+    return arr
   end
 
   def self.evaluate(rpn_list)
+    stack = Stack.new
+    rpn_list.each do |item|
+      if RPNCalculator.is_number(item)
+        stack.push(item.to_f)
+      end
+      if RPNCalculator.is_operation(item)
+        x = stack.pop
+        y = stack.pop
+        stack.push(y.method(item).(x))
+      end
+    end
+    return stack.pop
   end
 
   def self.is_operation(operation)
+    if ["+", "-", "^", "*", "/"].include?(operation)
+      return true
+    end
+    return false
   end
-  
+
   def self.is_number(num)
+    if num.match(/[-]?\d+/)
+      return true
+    end
+    return false
   end
+
 end
+
+
+
--- stack.rb
+++ stack.rb
@@ -1,5 +1,53 @@
-# The stack should be implemented using a linked list.
-# Do not use any ruby arrays!!
-
 class Stack
+  attr_reader :length
+  attr_accessor :head, :tail
+
+  def initialize
+    @head = nil
+    @tail = nil
+    @length = 0
+  end
+
+  # Adds a new value to the end of the list.
+  def push(value)
+    if @head == nil
+      @head = Node.new(value)
+      @tail = @head
+    else
+      old_tail = @tail
+      @tail = Node.new(value)
+      old_tail.next = @tail
+      @tail.previous = old_tail
+    end
+    @length += 1
+    nil
+  end
+
+  def pop
+    return nil if @length == 0
+
+    if @length == 1
+      return_value = @tail.value
+      nil_node(@tail)
+      @tail = nil
+      @head = nil
+      @length = 0
+      return return_value
+    end
+
+    old_tail = @tail
+    @tail = old_tail.previous
+    @tail.next = nil
+
+    return_value = old_tail.value
+
+    @length -= 1
+
+    nil_node(old_tail)
+
+    return_value
+  end
+
+
+
 end
